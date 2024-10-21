1a813497291d778974fdfbd0a7b256a71e727fae
Anni Tias <annitias1904@gmail.com>
2024-10-21 13:59:56 +0700

    fix

---
+++ bootstrap-5-admin-dashboard-template-main/js/script.js
@@ -0,0 +1,94 @@
+const charts = document.querySelectorAll(".chart");
+
+charts.forEach(function (chart) {
+  var ctx = chart.getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Whiskas", "Bolt", "Cat Choize", "Excel", "Meo"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [50, 75, 30, 20, 40],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".data-table").each(function (_, table) {
+    $(table).DataTable();
+  });
+});
+const charts2 = document.querySelectorAll(".chart2");
+
+charts2.forEach(function (chart) {
+  var ctx = chart.getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [50, 75, 30, 20, 40, 80, 100, 45, 60, 90, 0, 0],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $(".data-table").each(function (_, table) {
+    $(table).DataTable();
+  });
+});
